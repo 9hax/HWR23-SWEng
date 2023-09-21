ed6d1d43d34f4564e1a76c71543015fe86eb1a23
Anna Stabel <annastabel85@gmail.com>
2023-09-21 19:12:50 +0200

    new foreign keys added, models renamed

--- models.py
+++ models.py
@@ -12,10 +12,13 @@ class User(db.Model):
     highPermissionLevel = db.Column(db.Boolean, unique=False, nullable=False, default=False)
     isOffice = db.Column(db.Boolean, unique=False, nullable=False, default=False)
     userData = db.Column(db.Text, unique=False, nullable=True)
+    documentID = db.Column(db.Integer, db.ForeignKey('document.id'))
+    
+    userDocument = db.relationship('Document', backref='existing documents', foreign_keys=[documentID])
     def __repr__(self):
         return '<User %r>' % self.username
 
-class Ticket(db.Model):
+class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(512), unique=False, nullable=True)
     is_open = db.Column(db.Boolean, unique=False, nullable = False, default= True)
@@ -23,22 +26,26 @@ class Ticket(db.Model):
     media = db.Column(db.Text, unique=False, nullable=True) #This contains base64'ed binary images and videos in a python list.
     time = db.Column(db.Integer, unique = False) # The time the ticket was created in epoch seconds
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    send_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     hidden = db.Column(db.Boolean, unique=False, default= False)
 
     created_by = db.relationship('User', backref='tickets_created_by', foreign_keys=[created_by_id])
+    send_to = db.relationship('Office', backref='tickets_created_by', foreign_keys=[created_by_id])
     def __repr__(self):
         return '<Ticket %r>' % self.title
 
-class TicketReply(db.Model):
+class DocumentReply(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     text = db.Column(db.Text, unique=False, nullable=False)
     media = db.Column(db.Text, unique=False, nullable=True)
     isNote = db.Column(db.Boolean, unique=False, nullable = True)
     time = db.Column(db.Integer, unique = False) # The time the ticket reply was created in epoch seconds
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    send_to_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     main_ticket_id = db.Column(db.Integer, db.ForeignKey('ticket.id'))
 
     created_by = db.relationship('User', backref='ticket_reply_by', foreign_keys=[created_by_id])
+    send_to = db.relationship('User', backref='ticket_reply_by', foreign_keys=[created_by_id])
     main_ticket = db.relationship('Ticket', backref = 'ticket_reply_main_ticket', foreign_keys=[main_ticket_id])
     def __repr__(self):
         return '<TicketReply to %r>' % self.main_ticket
