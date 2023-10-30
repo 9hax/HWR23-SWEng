8e191f217773f8acbc0e4c1b4bddb88d7f59d09e
Paul Vierkorn <me@9hax.net>
2023-10-30 19:16:12 +0100

    remove old git library

--- simpleticket.py
+++ simpleticket.py
@@ -16,17 +16,13 @@ try:
 except: 
     import config
 
-import json, user, git, os, time, datetime
+import json, user, os, time, datetime
 
 # prepare language files
 with open("lang/"+config.LANGUAGE+".json",'r',encoding="utf-8") as langfile:
     lang = json.load(langfile)
 
-# get current git commit shortname to display in the about page
-try:
-    version = git.Repo(search_parent_directories=True).head.object.hexsha[0:7]
-except:
-    version = "Git Error"
+version = "Git Error"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
