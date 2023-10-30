cc625c08ce3bdab0a1efa2a7f8935c95713ee7d2
Paul Vierkorn <me@9hax.net>
2023-10-30 18:49:58 +0100

    add autorun batch file for windows users on local instances

---
+++ run.bat
@@ -0,0 +1,34 @@
+@echo off
+echo "EasyFormular Instance Runner"
+
+REM Upgrade pip version
+py -m pip install --upgrade pip
+
+REM Install Pip Requirements
+py -m pip install -r requirements.txt
+
+cls
+
+echo Done Installing Libraries.
+echo.
+echo Preparing Database.
+py -m flask db init
+
+echo Creating Database Content.
+py -m flask db migrate
+py -m flask db upgrade
+py -m flask db migrate
+py -m flask db upgrade
+py -m flask db migrate
+py -m flask db upgrade
+py -m flask db migrate
+py -m flask db upgrade
+
+cls
+
+echo Run the web Server...
+echo Open your browser at http://localhost:8123 to access the application.
+
+echo starting up flask...
+
+py -m flask run --port=8123
